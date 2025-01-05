123fef2ab4623ff94d714f869f7c392771a16cbd
Venagalashaurav <vengalashaurav23@gmail.com>
2025-01-05 15:58:14 +0000

    SOME changes to the website
    added banner, sub-title:OUR BEST FOODS,Added random food icons.

--- website.py
+++ website.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-  return "This is my first project"
+  return render_template('web.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
